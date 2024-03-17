1845bd9aa3dc75b27648bbacb59579e7d62a13fb
Karimjon <okracer3350@gmail.com>
2024-03-17 10:29:12 +0500

    Add some cliens and interfaces

---
+++ src/interfaces/Pages/IPage.cs
@@ -0,0 +1,12 @@
+﻿namespace interfaces.Pages;
+
+public interface IPage<out T>
+{
+    IEnumerable<T> Items { get; }
+    int Index { get; }
+    int Count { get; }
+    int TotalItems { get; }
+    int TotalPages { get; }
+    string SortedBy { get; }
+    string Filter { get; }
+}
---
+++ src/interfaces/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using interfaces.Pages;
+
+namespace interfaces.Repositories;
+
+public interface IRepository<T>
+{
+    Task<T> PostAsync(T model, CancellationToken token = default);
+    Task<T> PostAsync(IEnumerable<T> models, CancellationToken token = default);
+    
+    Task<T> DeleteAsync(T model, CancellationToken token = default);
+    Task<T> DeleteAsync(int id, CancellationToken token = default);
+    Task<T> DeleteAsync(IEnumerable<int> ids, CancellationToken token = default);
+    
+    Task<T> PatchAsync(int id, T model, CancellationToken token = default);
+    Task<T> PutAsync(int id, T model, CancellationToken token = default);
+    
+    Task<T> GetAsync(int id, CancellationToken token = default);
+    Task<IEnumerable<T>> GetAsync(CancellationToken token = default);
+    
+    Task<IPage<T>> GetAscAsync(int index, int count, string sortBy, string filter, CancellationToken token = default);
+    Task<IPage<T>> GetDescAsync(int index, int count, string sortBy, string filter, CancellationToken token = default);
+
+    Task<bool> Exists(int id, CancellationToken token);
+}
